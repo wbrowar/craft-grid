75a5780d69f0b5821afd6432dc900b9e54009b89
Will Browar <wbrowar@gmail.com>
2019-03-24 16:33:24 -0400

    Fixed migration error

--- src/migrations/m190311_000515_add_layout_id.php
+++ src/migrations/m190311_000515_add_layout_id.php
@@ -15,30 +15,33 @@ class m190311_000515_add_layout_id extends Migration
      */
     public function safeUp()
     {
-        // Find each instance of the Grid field and add an `id` property
-        $allFields = Craft::$app->getFields()->getAllFields();
+        // Check if the updates can be made
+        if (Craft::$app->getConfig()->getGeneral()->allowAdminChanges) {
+            // Find each instance of the Grid field and add an `id` property
+            $allFields = Craft::$app->getFields()->getAllFields();
 
-        foreach ($allFields as $field) {
-            if (get_class($field) == 'wbrowar\grid\fields\Grid') {
-                $layout = json_decode($field->layout);
-                $breakpoints = $layout->breakpoints;
+            foreach ($allFields as $field) {
+                if (get_class($field) == 'wbrowar\grid\fields\Grid') {
+                    $layout = json_decode($field->layout);
+                    $breakpoints = $layout->breakpoints;
 
-                foreach ($breakpoints as &$breakpoint) {
-                    // Create `id` from `minWidth` in the old pattern
-                    $breakpoint->id = 'id' . $breakpoint->minWidth;
+                    foreach ($breakpoints as &$breakpoint) {
+                        // Create `id` from `minWidth` in the old pattern
+                        $breakpoint->id = 'id' . $breakpoint->minWidth;
 
-                    // Remove any `prevMinWidth` properties
-                    if ($breakpoint->prevMinWidth ?? false) {
-                        unset($breakpoint->prevMinWidth);
+                        // Remove any `prevMinWidth` properties
+                        if ($breakpoint->prevMinWidth ?? false) {
+                            unset($breakpoint->prevMinWidth);
+                        }
                     }
+
+                    $layout->breakpoints = $breakpoints;
+
+                    $newField = $field;
+                    $newField->layout = json_encode($layout);
+
+                    Craft::$app->getFields()->saveField($newField);
                 }
-
-                $layout->breakpoints = $breakpoints;
-
-                $newField = $field;
-                $newField->layout = json_encode($layout);
-
-                Craft::$app->getFields()->saveField($newField);
             }
         }
     }
